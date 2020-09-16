0b621a380960e71d9980015fc04ac170a7ef859a
avivgood <51792766+avivgood@users.noreply.github.com>
2020-09-16 18:56:06 +0300

    Add files via upload

---
+++ Keyboard/Keyboard.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Keyboard.aspx.cs" Inherits="Keyboard.Keyboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #Text1 {
+            height: 137px;
+            width: 1447px;
+        }
+        #input {
+            width: 1487px;
+            height: 142px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Text" runat="server" OnLoad="Text_Load" Text="Label"></asp:Label>
+        <asp:Panel ID="Btns" runat="server" Height="691px">
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Keyboard/Keyboard.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Keyboard
+{
+    public partial class Keyboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            ICollection<Button> buttonsLayout = new List<Button>();
+            for (char current = 'a'; current <= 'z'; current++)
+            {
+                Button currentBtn = new Button();
+                currentBtn.Text = current.ToString();
+                currentBtn.Click += addToText;
+                currentBtn.Width = 100;
+                currentBtn.Height = 100;
+                currentBtn.ID = current.ToString();
+                Btns.Controls.Add(currentBtn);
+                currentBtn.Visible = true;
+                currentBtn.BackColor = Color.AliceBlue;
+                buttonsLayout.Add(currentBtn);
+            }
+
+        }
+
+        private void addToText(object sender, EventArgs e)
+        {
+            var btn = (Button) sender;
+            Text.Text += btn.Text;
+        }
+
+        protected void Text_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
